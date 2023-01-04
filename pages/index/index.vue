50e05adf3746e067bc612ee8fbf11705decab9e7
cps8 <806637468@qq.com>
2023-01-04 18:06:34 +0800

    完善使用示例

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,30 +1,32 @@
 <template>
-	<text>多项选择器：</text>
-	<multiple-picker :title="title" :items="items" :disabled="disabled" :show="show" :values="value" @confrim="onConfrim"/>
+	<view style="position: relative;">
+		<multiple-picker :title="title" :items="items" :disabled="disabled" :show="show" :values="value" color="#ff0000" okColor="#ff3333"
+			:multiple="false" @confrim="onConfrim" />
+	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: "请选择",
-				items: [],
-				disabled: false,
-				show: false,
-				value: []
-			}
-		},
-		onLoad() {
+export default {
+	data() {
+		return {
+			title: "请选择",
+			items: [{ code: "001", text: "选项1" }, { code: "002", text: "选项2" }, { code: "003", text: "选项3" }],
+			disabled: false,
+			show: false,
+			value: ""
+		}
+	},
+	onLoad() {
 
-		},
-		methods: {
-			onConfrim(event){
-				console.log(event);
-			}
+	},
+	methods: {
+		onConfrim(event) {
+			// console.log(event);
 		}
 	}
+}
 </script>
 
 <style lang="scss" scoped>
-	@import url("./index.scss");
+@import url("./index.scss");
 </style>
